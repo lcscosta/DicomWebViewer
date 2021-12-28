51389b9db8e3325edbff0a06814e5fd6d3b4e24b
lcscosta <lucasdacosta@usp.br>
2021-12-27 21:04:51 -0300

    Added Search Page

---
+++ src/Search.js
@@ -0,0 +1,14 @@
+import './Search.css';
+
+function Search() {
+  return (
+    <div className="Search">
+      <header className="Search-header">
+        <title>DicomWebViewer</title>
+        <h1>DicomWebViewer App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Search;
